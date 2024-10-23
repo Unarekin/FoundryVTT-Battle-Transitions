2f9d23e26548e1b681dd9fcae5f68ace445811c0
Erica Phelps <erica@blackspork.com>
2024-10-23 15:04:50 -0500

    feat:Added bilinear wipe configuration

---
+++ src/config/BilinearWipeConfigHandler.ts
@@ -0,0 +1,51 @@
+import { TransitionConfigHandler } from "../interfaces";
+import { localize } from "../utils";
+import { BilinearDirection, RadialDirection } from '../types';
+
+export interface BilinearWipeConfiguration {
+  duration: number;
+  direction: BilinearDirection;
+  radial: RadialDirection;
+  background: string;
+  id?: string;
+}
+
+export class BilinearWipeConfigHandler implements TransitionConfigHandler<BilinearWipeConfiguration> {
+  public get key() { return "bilinear-wipe"; }
+  public get name() { return "BATTLETRANSITIONS.TRANSITIONTYPES.BILINEARWIPE"; }
+  public readonly defaultSettings: BilinearWipeConfiguration = {
+    duration: 1000,
+    direction: "horizontal",
+    radial: "inside",
+    background: "#00000000",
+  }
+
+  public generateSummary(flag?: BilinearWipeConfiguration): string {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    if (flag) return [localize("BATTLETRANSITIONS.FORMATTERS.MILLISECONDS", { value: (flag as any).duration }), flag.direction, flag.radial, (flag as any).background].join("; ");
+    else return "";
+  }
+
+  public renderTemplate(flag?: BilinearWipeConfiguration): Promise<string> {
+    return renderTemplate(`/modules/${__MODULE_ID__}/templates/config/bilinear-wipe-config.hbs`, flag ?? {});
+  }
+
+  public createFlagFromHTML(html: HTMLElement | JQuery<HTMLElement>): BilinearWipeConfiguration {
+    const form = $(html).find("form").serializeArray();
+
+    const duration = form.find(elem => elem.name === "duration");
+    const background = form.find(elem => elem.name === "background");
+    const direction = form.find(elem => elem.name === "direction");
+    const radial = form.find(elem => elem.name === "radial");
+    const id = form.find(elem => elem.name === "id");
+
+    return {
+      ...this.defaultSettings,
+      ...(duration ? { duration: parseFloat(duration.value) } : {}),
+      ...(background ? { background: background.value } : {}),
+      ...(direction ? { direction: direction.value as BilinearDirection } : {}),
+      ...(radial ? { radial: radial.value as RadialDirection } : {}),
+      ...(id ? { id: id.value } : { id: foundry.utils.randomID() })
+    };
+  }
+}
