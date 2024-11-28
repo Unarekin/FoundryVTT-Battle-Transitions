71dc46407fbd1acf4687e8d232517e6aae67d804
Erica Phelps <erica@blackspork.com>
2024-11-27 19:59:45 -0600

    feat:Added reverse to fade

--- src/steps/FadeStep.ts
+++ src/steps/FadeStep.ts
@@ -5,7 +5,9 @@ import { TransitionStep } from "./TransitionStep";
 import { FadeConfiguration } from "./types";
 
 export class FadeStep extends TransitionStep<FadeConfiguration> {
-  // #region Properties (7)
+  // #region Properties (9)
+
+  #filter: FadeTransitionFilter | null = null;
 
   public static DefaultSettings: FadeConfiguration = {
     id: "",
@@ -23,9 +25,10 @@ export class FadeStep extends TransitionStep<FadeConfiguration> {
   public static icon = "<i class='bt-icon fade fa-fw fas'></i>"
   public static key = "fade";
   public static name = "FADE";
+  public static reversible: boolean = true;
   public static template = "fade-config";
 
-  // #endregion Properties (7)
+  // #endregion Properties (9)
 
   // #region Public Static Methods (7)
 
@@ -62,7 +65,7 @@ export class FadeStep extends TransitionStep<FadeConfiguration> {
 
   // #endregion Public Static Methods (7)
 
-  // #region Public Methods (1)
+  // #region Public Methods (2)
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public async execute(container: PIXI.Container, sequence: TransitionSequence): Promise<void> {
@@ -72,9 +75,14 @@ export class FadeStep extends TransitionStep<FadeConfiguration> {
     }
     const background = config.deserializedTexture ?? createColorTexture("transparent");
     const filter = new FadeTransitionFilter(background.baseTexture);
+    this.#filter = filter;
     this.addFilter(container, filter);
     await this.simpleTween(filter);
   }
 
-  // #endregion Public Methods (1)
+  public async reverse(): Promise<void> {
+    if (this.#filter instanceof FadeTransitionFilter) await this.simpleReverse(this.#filter);
+  }
+
+  // #endregion Public Methods (2)
 }
