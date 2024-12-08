b3fff80618b821ef902cc70e15979c66d8e73d8b
Erica Phelps <erica@blackspork.com>
2024-12-08 05:36:40 -0600

    feat:Updating configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,13 +25,14 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    baseURL: "http://localhost:20000",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: {
-      mode: "on",
+      mode: "retain-on-failure",
       size: { width: 1920, height: 1080 }
     }
   },
--- tests/easings.spec.ts
+++ tests/easings.spec.ts
@@ -2,15 +2,47 @@ import { test } from "@playwright/test";
 import { setupWorld } from "./testFunctions"
 import { configureScene } from "./foundryFunctions";
 
-test("Angular Wipe", async ({ page }) => {
-  await setupWorld(page, "battle-transitions-v12");
+interface WipeSpec {
+  name: string;
+  key: string;
+}
 
-  await page.evaluate(() => game?.scenes?.getName("Scene 1")?.activate())
+const wipes: WipeSpec[] = [
+  { name: "Angular Wipe", key: "angularwipe" },
+  { name: "Bar Wipe", key: "barwipe" },
+  { name: "Bilinear Wipe", key: "bilinearwipe" },
+  { name: "Clock Wipe", key: "clockwipe" },
+  { name: "Diamond Wipe", key: "diamondwipe" },
+  { name: "Fade", key: "fade" },
+  { name: "Fire Dissolve", key: "firedissolve" },
+  { name: "Hue Shift", key: "hueshift" },
+  { name: "Linear Wipe", key: "linearwipe" },
+  { name: "Melt", key: "melt" },
+  { name: "Pixelate", key: "pixelate" },
+  { name: "Radial Wipe", key: "radialwipe" },
+  { name: "Spiral Shutter", key: "spiralshutter" },
+  { name: "Spiral Wipe", key: "spiralwipe" },
+  { name: "Spotlight Wipe", key: "spotlightwipe" },
+  { name: "Twist", key: "twist" },
+  { name: "Wave Wipe", key: "wavewipe" },
+  { name: "Zoom & Blur", key: "zoomblur" },
+  { name: "Zoom", key: "zoom" }
+];
+const easings: string[] = [
+  ...[
+    "power1", "power2", "power3", "power4",
+    "back", "bounce", "circ", "elastic", "expo", "sine"
+  ].map(easing => [`${easing}.in`, `${easing}.out`, `${easing}.inOut`]).flat(),
+  "steps", "rough", "slow", "expoScale"
+]
 
-  await configureScene(page, "Scene 1");
-  // await page.evaluate(async () => {
-  //   await new BattleTransition("Scene 2")
-  //     .linearWipe("left")
-  //     .execute();
-  // });
+
+wipes.forEach(wipe => {
+  test.describe(wipe.name, () => {
+    easings.forEach(easing => {
+      test(easing, async ({ page }) => {
+
+      })
+    })
+  })
 })
