48316bf1b6bd2e06d16091523a6ef046349bd73a
Erica Phelps <erica@blackspork.com>
2024-12-11 00:44:29 -0600

    feat:Added BarWipeMigrator, HueShiftMIgrator, PixelateMigrator to transition step migrator hash

--- src/migration/TransitionSteps.ts
+++ src/migration/TransitionSteps.ts
@@ -1,18 +1,21 @@
-import { AngularWipeMigrator, BilinearWipeMigrator, ClockWipeMigrator, DiamondWipeMigrator, FadeMigrator, FireDissolveMigrator, FlashMigrator, InvertMigrator, LinearWipeMigrator, MacroMigrator, MeltMigrator, ParallelMigrator, RadialWipeMigrator, RemoveOverlayMigrator, RestoreOverlayMigrator, SceneChangeMigrator, SoundMigrator, SpiralShutterMigrator, SpiralWipeMigrator, SpotlightWipeMigrator, TextureSwapMigrator, VideoMigrator, WaitMigrator, WaveWipeMigrator } from "./steps";
+import { AngularWipeMigrator, BarWipeMirator, BilinearWipeMigrator, ClockWipeMigrator, DiamondWipeMigrator, FadeMigrator, FireDissolveMigrator, FlashMigrator, HueShiftMigrator, InvertMigrator, LinearWipeMigrator, MacroMigrator, MeltMigrator, ParallelMigrator, PixelateMigrator, RadialWipeMigrator, RemoveOverlayMigrator, RestoreOverlayMigrator, SceneChangeMigrator, SoundMigrator, SpiralShutterMigrator, SpiralWipeMigrator, SpotlightWipeMigrator, TextureSwapMigrator, VideoMigrator, WaitMigrator, WaveWipeMigrator } from "./steps";
 
 const TransitionSteps = {
   angularwipe: new AngularWipeMigrator(),
+  barwipe: new BarWipeMirator(),
   bilinearwipe: new BilinearWipeMigrator(),
   clockwipe: new ClockWipeMigrator(),
   diamondwipe: new DiamondWipeMigrator(),
   fade: new FadeMigrator(),
   firedissolve: new FireDissolveMigrator(),
   flash: new FlashMigrator(),
+  hueshift: new HueShiftMigrator(),
   invert: new InvertMigrator(),
   linearwipe: new LinearWipeMigrator(),
   macro: new MacroMigrator(),
   melt: new MeltMigrator(),
   parallel: new ParallelMigrator(),
+  pixelate: new PixelateMigrator(),
   radialwipe: new RadialWipeMigrator(),
   removeoverlay: new RemoveOverlayMigrator(),
   restoreoverlay: new RestoreOverlayMigrator(),
