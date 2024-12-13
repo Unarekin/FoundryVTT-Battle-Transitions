fbb588726b9a1bf720d51d5a2f8685655ca98059
Erica Phelps <erica@blackspork.com>
2024-12-13 06:02:19 -0600

    fix:Fixed misspelling of clockDirection form element

--- src/steps/ClockWipeStep.ts
+++ src/steps/ClockWipeStep.ts
@@ -68,7 +68,7 @@ export class ClockWipeStep extends TransitionStep<ClockWipeConfiguration> {
 
   public static fromFormElement(form: HTMLFormElement): ClockWipeStep {
     const backgroundImage = $(form).find("#backgroundImage").val() as string ?? "";
-    const elem = parseConfigurationFormElements($(form) as JQuery<HTMLFormElement>, "id", "duration", "direction", "clockdirection", "easing", "backgroundType", "backgroundColor", "label");
+    const elem = parseConfigurationFormElements($(form) as JQuery<HTMLFormElement>, "id", "duration", "direction", "clockDirection", "easing", "backgroundType", "backgroundColor", "label");
 
     return new ClockWipeStep({
       ...ClockWipeStep.DefaultSettings,
