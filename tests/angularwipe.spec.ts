ec8163cf9024eb78d5201644a571120cf908d014
Erica Phelps <erica@blackspork.com>
2024-12-13 20:02:01 -0600

    feat:Added visual testing

--- tests/angularwipe.spec.ts
+++ tests/angularwipe.spec.ts
@@ -1,7 +1,10 @@
-import { testv12 as test, expect } from "./fixtures";
-import { angularwipe } from "./data/defaults.json"
+import { testv12 as test } from "./fixtures";
+import { angularwipe } from "./data/defaults.json";
 import easings from "./data/easings.json";
 import { backgroundColorTest, backgroundImageTest, backgroundOverlayTest, defaultConfigurationTest, durationTest, easingTest, labelTest } from "./configTestFunctions";
+import { LinearWipeConfiguration } from "../src/steps";
+
+declare const BattleTransition: any;
 
 test.describe("Configuration Tests", () => {
   test("Can set default configuration", async ({ page }) => {
@@ -58,4 +61,62 @@ test.describe("Configuration Tests", () => {
     }
   });
 
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  test.describe("Visuals", () => {
+    const directions = ["left", "topleft", "top", "topright", "right", "bottomright", "bottom", "bottomleft"];
+    const config = directions.map(direction => ({
+      id: "",
+      version: "1.1.6",
+      type: "linearwipe",
+      duration: 1000,
+      direction,
+      backgroundColor: `${getRandomColor()}FF`,
+      backgroundType: "color",
+      bgSizingMode: "stretch",
+      easing: "none"
+    }));
+
+    test("Aw yee", async ({ page }) => {
+
+      await page.evaluate<void, typeof config>(async (config) => {
+
+        await BattleTransition
+          .ExecuteSequence("Scene 2", config)
+          .then(() => new Promise(resolve => { setTimeout(resolve, 2000) }))
+          // .then(() => new Promise(resolve => { setTimeout(resolve, config.length * 750) }))
+          .then(() => game?.scenes?.getName("Scene 1").activate())
+          ;
+
+      }, config);
+
+    });
+
+    // test("Left", async ({ page }) => {
+    //   await page.evaluate(() => BattleTransition.ExecuteSequence(
+    //     game?.scenes?.getName("Scene 2") as Scene,
+    //     [
+    //       {
+    //         id: "",
+    //         version: "1.1.6",
+    //         type: "linearwipe",
+    //         duration: 2000,
+    //         direction: "left",
+    //         backgroundColor: "#00000000",
+    //         backgroundType: "color",
+    //         bgSizingMode: "stretch",
+    //         easing: "none"
+    //       } as LinearWipeConfiguration,
+    //     ]
+    //   )
+    //     .then(() => new Promise(resolve => { setTimeout(resolve, 2000) })));
+
+  })
 });
