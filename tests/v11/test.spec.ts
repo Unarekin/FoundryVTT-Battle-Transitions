deeeb476abf87610d5ecfc5d64b977cd80ab88cf
Erica Phelps <erica@blackspork.com>
2024-12-19 01:48:33 -0600

    feat:Added angularwipe v11 spec

--- tests/v11/test.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from "../fixtures";
-
-test("Test", async ({ page }) => {
-  // await page.waitForLoadState("load");
-  await new Promise(resolve => { setTimeout(resolve, 5000) })
-});
