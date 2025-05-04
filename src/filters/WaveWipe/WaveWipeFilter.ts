fb332230a2c53b58c79f9d47093a2317e1eb6c95
Erica Phelps <erica@blackspork.com>
2025-05-03 19:27:07 -0500

    feat:Implements #50; add feathered edge to wave wipe

--- src/filters/WaveWipe/WaveWipeFilter.ts
+++ src/filters/WaveWipe/WaveWipeFilter.ts
@@ -4,11 +4,11 @@ import { TextureWipeFilter } from '../TextureWipe/TextureWipeFilter';
 import { RadialDirection } from '../../types';
 
 export class WaveWipeFilter extends TextureWipeFilter {
-  constructor(direction: RadialDirection, background: PIXI.TextureSource | PIXI.ColorSource) {
+  constructor(direction: RadialDirection, background: PIXI.TextureSource | PIXI.ColorSource, falloff: number) {
     const bg = coerceTexture(background);
     if (!(bg instanceof PIXI.Texture)) throw new InvalidTextureError();
 
     const wipeTexture = PIXI.Texture.from(`/modules/${__MODULE_ID__}/assets/wipes/wave-${direction === "inside" ? "inside" : "outside"}.webp`);
-    super(wipeTexture, bg);
+    super(wipeTexture, falloff, bg);
   }
 }
--- src/migration/steps/WaveWipe.ts
+++ src/migration/steps/WaveWipe.ts
@@ -4,13 +4,19 @@ import { v115EasingFix } from "./functions";
 
 export class WaveWipeMigrator extends Migrator<WaveWipeConfiguration> {
   protected migrationFunctions: { [x: string]: (old: any) => WaveWipeConfiguration } = {
+    "<2.0.0": (old: any) => ({
+      falloff: 0,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      ...v115EasingFix(old),
+      version: "2.0.0"
+    }),
     "<=1.1.5": (old: any) => ({
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       ...v115EasingFix(old),
       version: "1.1.6"
     })
   };
-  public readonly NewestVersion: string = "1.1.6";
+  public readonly NewestVersion: string = "2.0.0";
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   public Version(data: unknown): string { return ((data as any).version as string) ?? "1.0.5"; }
