5b8d376291efce5ee0cc309dc462d067d0db332a
Erica Phelps <erica@blackspork.com>
2025-05-19 23:15:15 -0500

    chore:Removing test suites

--- tests/v11/global.teardown.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "@playwright/test";
-import { downOne } from "docker-compose"
-import path from "path";
-
-test("Closing container", async () => {
-  await downOne("foundry11", { cwd: path.resolve(__dirname, "../..") });
-})
