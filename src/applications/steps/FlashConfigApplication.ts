bfcca8ece1121f9f65595e35367dc2816bc9f77f
Erica Phelps <erica@blackspork.com>
2025-09-14 06:16:36 -0500

    fix:Flash config wasn't respecting dual style, swapped from simple-select partial to dualstyle-selector partial

--- src/applications/steps/FlashConfigApplication.ts
+++ src/applications/steps/FlashConfigApplication.ts
@@ -13,12 +13,25 @@ export class FlashConfigApplication extends StepConfigApplication<FlashConfigura
         `modules/${__MODULE_ID__}/templates/steps/partials/label.hbs`,
         `modules/${__MODULE_ID__}/templates/steps/partials/duration-selector.hbs`,
         `modules/${__MODULE_ID__}/templates/steps/partials/background-selector.hbs`,
-        `modules/${__MODULE_ID__}/templates/steps/partials/simple-select.hbs`
+        `modules/${__MODULE_ID__}/templates/steps/partials/dualstyle-selector.hbs`
       ]
     },
     footer: {
       template: "templates/generic/form-footer.hbs"
     }
+  }
+
+  parseFormData(data: Record<string, unknown>): FlashConfiguration {
+    const parsed = super.parseFormData(data);
+
+    const anyParsed = parsed as unknown as Record<string, unknown>;
+    parsed.applyToOverlay = anyParsed.dualStyle === "both" || anyParsed.dualStyle === "overlay";
+    parsed.applyToScene = anyParsed.dualStyle === "both" || anyParsed.dualStyle === "scene";
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    delete (parsed as any).dualStyle;
+
+    return parsed;
   }
 
   protected async _prepareContext(options: foundry.applications.api.ApplicationV2.RenderOptions): Promise<FlashContext> {
