b6cf09ee2cc58772d1e72cc023c05abe3f58c77e
Erica Phelps <erica@blackspork.com>
2025-09-14 01:51:01 -0500

    feat:Removes scene config dialogs

--- src/dialogs/index.ts
+++ src/dialogs/index.ts
@@ -1,7 +1,4 @@
 export * from './EditSequence';
-export * from './SceneConfig';
-export * from './SceneConfigV1';
-export * from './SceneConfigV2';
 export * from './addStepFunctions';
 export * from './functions';
 export * from './types';
