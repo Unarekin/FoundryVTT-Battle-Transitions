d13d4ecf1769600887894091b79b091bd977d60b
Erica Phelps <erica@blackspork.com>
2025-09-15 08:49:36 -0500

    feat:Added fade out music / fade transition example

---
+++ src/macros/Fade Transition.js
@@ -0,0 +1,21 @@
+try {
+  const scene = await BattleTransition.SelectScene(true);
+  if (!(scene instanceof Scene)) return;
+
+  const { fadeIn = 1000, duration = 2000, fadeOut = 1000 } = scope;
+
+  const macro = await fromUuid("Macro.sEHGeD83Dv5POL4P");
+  if (macro instanceof Macro) await macro.execute({ duration: 500 });
+
+  const screenWidth =
+    window.innerWidth - $("#ui-left").width() - $("#ui-right").width() - 100;
+
+  await new BattleTransition(scene)
+    .fade(fadeIn, "black")
+    .wait(duration)
+    .startPlaylist()
+    .fade(fadeOut)
+    .execute();
+} catch (err) {
+  ui.notifications.error(err.message);
+}
